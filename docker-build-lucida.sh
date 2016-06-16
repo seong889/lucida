c6650a9bb37313ac2a31304d771f221ef4f09e5f
Seongjun Hong <seong889@gmail.com>
2016-06-17 00:35:04 +0900

    add Docker build script

---
+++ docker-build-lucida.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+## setting lucida dependency
+cp Dockerfile.depend Dockerfile
+sudo nvidia-docker build -t lucida:depend .
+
+## git clone lucida denpendency library
+cp Dockerfile.clone Dockerfile
+sudo nvidia-docker build -t lucida:clone .
+
+## install lucida
+cp Dockerfile.install Dockerfile
+sudo nvidia-docker build -t lucida:install .
