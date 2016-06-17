3aa60034b3df2aa000aa220a841c13dcf84e3e6d
Seongjun Hong <seong889@gmail.com>
2016-06-17 17:04:39 +0900

    add wiki download code

--- docker-build-lucida.sh
+++ docker-build-lucida.sh
@@ -11,3 +11,7 @@ sudo nvidia-docker build -t lucida:clone .
 ## install lucida
 cp Dockerfile.install Dockerfile
 sudo nvidia-docker build -t lucida:install .
+
+## download wiki data
+cp Dockerfile.latest Dockerfile
+sudo nvidia-docker build -t lucida:latest .
